f65e60a74370fe890381d46433bc125b4f5cd470
holymolyRon <wjdfhs111@gmail.com>
2026-01-07 11:29:41 +0900

    05. NaN 구분

--- 자바스크립트/05-타입변환/실습.js
+++ 자바스크립트/05-타입변환/실습.js
@@ -114,8 +114,8 @@ console.log(myNumber.toString(16));
 
 // 색상 값(일상, 10진수) → 색상 코드(컴퓨터, 16진수) 변환
 const red = 255,
-  green = 120,
-  blue = 30;
+  green = 128,
+  blue = 0;
 
 const redHexValue = red.toString(16).padStart(2, "0");
 console.log(redHexValue);
@@ -144,6 +144,13 @@ console.log(tempHexCode);
 console.log(hexCode);
 
 // 색상 코드(컴퓨터, 16진수) → 색상 값(일상, 10진수) 변환
+const redHex = "ff",
+  greenHex = "80",
+  blueHex = "00";
+
+console.log(parseInt(redHex, 16));
+console.log(parseInt(greenHex, 16));
+console.log(parseInt(blueHex, 16));
 
 // --------------------------------------------------------------------------
 // 숫자 → 불리언 변환
@@ -152,14 +159,18 @@ console.log(hexCode);
 // --------------------------------------------------------------------------
 
 // 출력 결과: 0 → false
+console.log(Boolean(10));
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean(-1));
+console.log(Boolean(-10));
 
-// 출력 결과: 1 → true
-
-// 출력 결과: -1 → true
-
-// 출력 결과: 0.1 → true
-
-// 출력 결과: -0.1 → true
+// 이중 부정(!!) 연산자 활용 (Boolean() 대체)
+console.log(!!10);
+console.log(!!1);
+console.log(!!0);
+console.log(!!-1);
+console.log(!!-10);
 
 // --------------------------------------------------------------------------
 // 문자 → 불리언 변환
@@ -168,13 +179,26 @@ console.log(hexCode);
 // --------------------------------------------------------------------------
 
 // 출력 결과: '' → false
+console.log("");
+console.log(Boolean(""));
+console.log(!!"");
 
 // 출력 결과: ' ' → true
+console.log(" ");
+console.log(Boolean(" "));
+console.log(!!" ");
 
 // 출력 결과: '0' → true
+console.log("0");
+console.log(Boolean("0"));
+console.log(!!"0");
+
+console.log("false");
+console.log(Boolean("false"));
+console.log(!!"false");
 
 // --------------------------------------------------------------------------
-// 숫자가 아님(NaN): 숫자 연산 실패 시, 생성
+// 숫자가 아님(NaN): 숫자 연산 실패 시, NaN 생성
 // --------------------------------------------------------------------------
 // * Math.sqrt(-1)
 // * 숫자 + NaN
@@ -186,22 +210,34 @@ console.log(hexCode);
 // --------------------------------------------------------------------------
 
 // 출력 결과: Math.sqrt(-1) → NaN
+console.log(Math.sqrt(-1));
 
 // 출력 결과: 숫자 + NaN → NaN
+console.log(2 + parseFloat("the load"));
 
 // 출력 결과: undefined + undefined → NaN
+console.log(Number(undefined));
+let t, u;
+console.log(t + u);
 
 // 출력 결과: 문자 / 숫자 → NaN
+console.log("320px" / 4);
+console.log(parseInt("320px", 10) / 4 + "px");
 
 // 출력 결과: Number(str) → NaN
+console.log(Number("수요일"));
 
 // 출력 결과: parseInt(str) → NaN
+console.log(parseInt("오전 11시", 10));
 
 // 출력 결과: parseFloat(str) → NaN
+console.log(parseFloat("배터리 방전"));
 
 // 출력 결과: 0 / 0 → NaN
+console.log(0 / 0);
 
 // 출력 결과: Infinity - Infinity → NaN
+console.log(Infinity - Infinity);
 
 // --------------------------------------------------------------------------
 // NaN 확인
@@ -211,29 +247,47 @@ console.log(hexCode);
 // * Number.isNaN(): 진짜 NaN만 판단 (정확) ✅
 // --------------------------------------------------------------------------
 
+// 비교 연산자로 NaN을 구분하는 것은 불가능!
+const thisIsNaN = Math.sqrt(-1);
+console.log(thisIsNaN);
+
+console.log(thisIsNaN == NaN);
+console.log(thisIsNaN === NaN);
+
 // 출력 결과: typeof NaN → 'number' (⚠️ NaN이지만, number 타입으로 인식)
+// 무용지물...
+console.log(typeof thisIsNaN);
 
 // 출력 결과: isNaN(1) → false (✅ NaN 아님)
+console.log(isNaN(1));
 
 // 출력 결과: isNaN('3') → false (✅ NaN 아님)
+console.log(isNaN("3"));
 
 // 출력 결과: isNaN([]) → false (✅ NaN 아님)
+console.log(isNaN([]));
 
 // 출력 결과: isNaN(null) → false (✅ NaN 아님)
+console.log(isNaN(null));
 
 // 출력 결과: isNaN(NaN) → true (✅ NaN 맞음)
+console.log(isNaN(NaN));
 
 // 출력 결과: isNaN('3n') → true (⚠️ NaN 맞음? 부정확한 이유: 타입 변환 후, 검사)
+console.log(isNaN("3n"));
 
 // 출력 결과: isNaN(undefined) → true (⚠️ NaN 맞음?)
+console.log(isNaN(undefined));
 
 // 출력 결과: isNaN({}) → true (⚠️ NaN 맞음?)
+console.log(isNaN({}));
 
 // 출력 결과: Number.isNaN('3n') → false (✅ NaN 아님, 정확한 이유: 타입 변환 없이 검사)
-
+console.log(Number.isNaN("3n"));
 // 출력 결과: Number.isNaN(undefined) → false (✅ NaN 아님)
-
+console.log(Number.isNaN(undefined));
 // 출력 결과: Number.isNaN({}) → false (✅ NaN 아님)
+console.log(Number.isNaN({}));
 
 // --------------------------------------------------------------------------
 // Falsy & Truthy
